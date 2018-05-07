167e8d5374c2c73a47d44ac884c2b09e53bce010
leon <jianliangzhang@ctrip.com>
2018-05-07 16:34:17 +0800

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import Button from 'antd/lib/button'
+import { Alert, Breadcrumb } from 'antd'
 import * as React from 'react'
+import { Link, Route, Switch, withRouter } from 'react-router-dom'
 import './App.css'
 
-import logo from './logo.svg'
+const App = () => (
+  <ul className="app-list">
+    <li>
+      <Link to="/apps/1">Application1</Link>：<Link to="/apps/1/detail">Detail</Link>
+    </li>
+    <li>
+      <Link to="/apps/2">Application2</Link>：<Link to="/apps/2/detail">Detail</Link>
+    </li>
+  </ul>
+)
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button type="primary">Button</Button>
-      </div>
-    )
-  }
+const breadcrumbNameMap = {
+  '/apps': 'Application List',
+  '/apps/1': 'Application1',
+  '/apps/2': 'Application2',
+  '/apps/1/detail': 'Detail',
+  '/apps/2/detail': 'Detail',
 }
+const Home = withRouter(props => {
+  const { location } = props
+  const pathSnippets = location.pathname.split('/').filter(i => i)
+  const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
+    return (
+      <Breadcrumb.Item key={url}>
+        <Link to={url}>
+          {breadcrumbNameMap[url]}
+        </Link>
+      </Breadcrumb.Item>
+    )
+  })
+  const breadcrumbItems = [(
+    <Breadcrumb.Item key="home">
+      <Link to="/">Home</Link>
+    </Breadcrumb.Item>
+  )].concat(extraBreadcrumbItems)
 
-export default App
+  const HomePage = () => <span>Home Page</span>
+
+  return (
+    <div className="demo">
+      <div className="demo-nav">
+        <Link to="/">Home</Link>
+        <Link to="/apps">Application List</Link>
+      </div>
+      <Switch>
+        <Route path="/apps" component={App} />
+        <Route render={HomePage} />
+      </Switch>
+      <Alert style={{ margin: '16px 0' }} message="Click the navigation above to switch:" />
+      <Breadcrumb>
+        {breadcrumbItems}
+      </Breadcrumb>
+    </div>
+  )
+})
+
+export default Home
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root') as HTMLElement
 )
 registerServiceWorker()
