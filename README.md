8e6dde903d281fd47683a186ceb1cdbc8f3a1b5e
leon <jianliangzhang@ctrip.com>
2018-05-07 14:49:12 +0800

    antd

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # leon-ts-app
 tststs
+
+```
+  create-react-app leon-ts-app --scripts-version=react-scripts-ts
+  yarn add antd
+  yarn add react-app-rewired -D
+  yarn add ts-import-plugin -D // 按需加载
+  yarn add yarn add react-app-rewire-less -D
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import * as React from 'react';
-import './App.css';
+import Button from 'antd/lib/button'
+import * as React from 'react'
+import './App.css'
 
-import logo from './logo.svg';
+import logo from './logo.svg'
 
 class App extends React.Component {
   public render() {
@@ -14,9 +15,10 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type="primary">Button</Button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
