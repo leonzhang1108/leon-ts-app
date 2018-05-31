c5e096aef8d2e828929907eba96eaa18f4f08810
leon <jianliangzhang@ctrip.com>
2018-05-31 18:17:28 +0800

    update

--- src/pages/Matrix/Vector2Display.tsx
+++ src/pages/Matrix/Vector2Display.tsx
@@ -2,21 +2,13 @@
 import * as React from 'react'
 import Vector from './Vector'
 
-interface IProps { 
-  ventorList: number[][],
-  step: number,
-  width: number
-}
-
-class Vector2Display extends React.Component<IProps> {
-  render() {
-    const { step, ventorList, width } = this.props
-    return (
-      <div className='display' style={{width: step === 2 ? width : 0, opacity: step === 2 ? 1 : 0}}>
-        <Vector ventorList={ventorList}/>
-      </div>
-    )
-  }
-}
+const Vector2Display = props => (
+  <div className='display' style={{
+    width: props.step === 2 ? props.width : 0, 
+    opacity: props.step === 2 ? 1 : 0
+  }}>
+    <Vector ventorList={props.ventorList}/>
+  </div>
+)
 
 export default Vector2Display
--- src/pages/Matrix/Vector3.tsx
+++ src/pages/Matrix/Vector3.tsx
@@ -2,19 +2,10 @@
 import * as React from 'react'
 import Vector from './Vector'
 
-interface IProps { 
-  ventorList: number[][] | undefined,
-  hilightList: number[][] | undefined
-}
-
-class Vector3 extends React.Component<IProps> {
-  render() {
-    return (
-      <div className='result'>
-        <Vector {...this.props}/>
-      </div>
-    )
-  }
-}
+const Vector3 = props => (
+  <div className='result'>
+    <Vector {...props}/>
+  </div>
+)
 
 export default Vector3
