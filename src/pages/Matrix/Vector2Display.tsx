4c5795cd0c56bcfc34a5e465b61c67bccc2acd2d
leon <jianliangzhang@ctrip.com>
2018-05-31 18:32:32 +0800

    update

--- src/pages/Matrix/Vector2Display.tsx
+++ src/pages/Matrix/Vector2Display.tsx
@@ -2,11 +2,13 @@
 import * as React from 'react'
 import Vector from './Vector'
 
+const getStyle = props => ({
+  width: props.step === 2 ? props.width : 0, 
+  opacity: props.step === 2 ? 1 : 0
+})
+
 const Vector2Display = props => (
-  <div className='display' style={{
-    width: props.step === 2 ? props.width : 0, 
-    opacity: props.step === 2 ? 1 : 0
-  }}>
+  <div className='display' style={getStyle(props)}>
     <Vector ventorList={props.ventorList}/>
   </div>
 )
