d9393173372dec63d17b89bd35e822829c377994
leon <jianliangzhang@ctrip.com>
2018-06-22 14:06:13 +0800

    吃饭

--- src/constant/menus.ts
+++ src/constant/menus.ts
@@ -2,20 +2,26 @@ export default [{
   route: 'home',
   icon: 'home',
   title: 'Home'
-}, {
-  icon: 'line-chart',
-  title: 'Echarts',
-  key: 'echarts',
-  childs: [{
-    icon: 'edit',
-    title: 'Sub Echarts',
-    key: 'sub-echarts',
-    childs: [{
-      route: 'pie-chart',
-      icon: 'pie-chart',
-      title: 'Pie Chart'
-    }]
-  }]
+},
+// {
+//   icon: 'line-chart',
+//   title: 'Echarts',
+//   key: 'echarts',
+//   childs: [{
+//     icon: 'edit',
+//     title: 'Sub Echarts',
+//     key: 'sub-echarts',
+//     childs: [{
+//       route: 'pie-chart',
+//       icon: 'pie-chart',
+//       title: 'Pie Chart'
+//     }]
+//   }]
+// }, 
+{
+  route: 'pie-chart',
+  icon: 'pie-chart',
+  title: 'Pie Chart'
 }, {
   route: 'matrix',
   icon: 'codepen',
