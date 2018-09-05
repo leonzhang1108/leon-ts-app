723e38486cdd6bb1f41c6d1f28e18908b2268a41
leonzhang <human1108@163.com>
2018-09-05 22:17:39 +0800

    update

--- src/pages/Calligraphy/index.tsx
+++ src/pages/Calligraphy/index.tsx
@@ -1,19 +1,18 @@
 
 import * as React from 'react'
 import './index.less'
-import imgSrc from '@img/fulei.png'
+import fulei from '@img/fulei.png'
+import bird from '@img/bird.jpg'
+import mountain from '@img/mountain.png'
 
 const renderSquare = () => (
   <table>
     <tbody>
       <tr>
-        <td/><td/><td/>
+        <td/><td/>
       </tr>
       <tr>
-        <td/><td/><td/>
-      </tr>
-      <tr>
-        <td/><td/><td/>
+        <td/><td/>
       </tr>
     </tbody>
   </table>
@@ -24,10 +23,11 @@ const Calligraphy = () => (
     <div className='title'>书汉字之韵 写墨香童年</div>
     <div className='person'>班级：<span/>&nbsp;&nbsp;姓名：<span/></div>
     <div className='calligraphy-content'>
-      <div className='anticon anticon-ts-app icon-brush'/>
-      <div className='anticon anticon-ts-app icon-teddy'/>
-      <img className='fulei-logo' src={imgSrc}/>
+      <img className='fulei-logo' src={fulei}/>
+      <img className='bird' src={bird}/>
+      <img className='mountain' src={mountain}/>
       <div className='row'>
+        { renderSquare() }
         { renderSquare() }
         { renderSquare() }
         { renderSquare() }
@@ -40,8 +40,18 @@ const Calligraphy = () => (
         { renderSquare() }
         { renderSquare() }
         { renderSquare() }
+        { renderSquare() }
       </div>
       <div className='row'>
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+      </div>
+      <div className='row'>
+        { renderSquare() }
         { renderSquare() }
         { renderSquare() }
         { renderSquare() }
