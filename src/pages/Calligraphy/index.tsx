8d794649d999dc08dbb6ff2b3e364f74c34c862e
leon <jianliangzhang@ctrip.com>
2018-09-05 17:43:20 +0800

    update

--- src/pages/Calligraphy/index.tsx
+++ src/pages/Calligraphy/index.tsx
@@ -1,58 +1,55 @@
 
 import * as React from 'react'
 import './index.less'
+import imgSrc from '@img/fulei.png'
 
-class Calligraphy extends React.Component {
+const renderSquare = () => (
+  <table>
+    <tbody>
+      <tr>
+        <td/><td/><td/>
+      </tr>
+      <tr>
+        <td/><td/><td/>
+      </tr>
+      <tr>
+        <td/><td/><td/>
+      </tr>
+    </tbody>
+  </table>
+)
 
-  renderSquare = () => (
-    <table>
-      <tbody>
-        <tr>
-          <td/><td/><td/>
-        </tr>
-        <tr>
-          <td/><td/><td/>
-        </tr>
-        <tr>
-          <td/><td/><td/>
-        </tr>
-      </tbody>
-    </table>
-  )
-
-  render () {
-    return (
-      <div className='calligraphy-wrapper'>
-        <div className='title'>书汉字之韵 写墨香童年</div>
-        <div className='person'>班级：<span/>&nbsp;&nbsp;姓名：<span/></div>
-        <div className='calligraphy-content'>
-          <div className='anticon anticon-ts-app icon-brush'/>
-          <div className='anticon anticon-ts-app icon-teddy'/>
-          <div className='row'>
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-          </div>
-          <div className='row'>
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-          </div>
-          <div className='row'>
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-            { this.renderSquare() }
-          </div>
-        </div>
+const Calligraphy = () => (
+  <div className='calligraphy-wrapper'>
+    <div className='title'>书汉字之韵 写墨香童年</div>
+    <div className='person'>班级：<span/>&nbsp;&nbsp;姓名：<span/></div>
+    <div className='calligraphy-content'>
+      <div className='anticon anticon-ts-app icon-brush'/>
+      <div className='anticon anticon-ts-app icon-teddy'/>
+      <img className='fulei-logo' src={imgSrc}/>
+      <div className='row'>
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
       </div>
-    )
-  }
-}
+      <div className='row'>
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+      </div>
+      <div className='row'>
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+        { renderSquare() }
+      </div>
+    </div>
+  </div>
+)
 
 export default Calligraphy
