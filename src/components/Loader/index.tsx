9dbec8de216d578f4eb42a5d82ae875f5bccda47
leonzhang <human1108@163.com>
2021-03-25 00:23:12 +0800

    update

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,20 +3,8 @@ import './index.less'
 
 const Loader = () => {
   return (
-    <div className="loading-center">
-      <div className="loading">
-        <div className="cat">
-          <div className="head">
-            <div className="ears"></div>
-            <div className="eyes"></div>
-          </div>
-        </div>
-        <div className="squares">
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-        </div>
-      </div>
+    <div className="loader-wrapper">
+      <div className="loader" />
     </div>
   )
 }
