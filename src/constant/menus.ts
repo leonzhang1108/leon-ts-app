6c6a438fb60ecfb33e02dfad7298e32287707888
leonzhang <human1108@163.com>
2021-03-25 00:37:54 +0800

    update

--- src/constant/menus.ts
+++ src/constant/menus.ts
@@ -9,11 +9,11 @@ export default [
     icon: 'ts-app icon-doraemon',
     title: '刘发财生日快乐！'
   },
-  {
-    route: 'loading-test',
-    icon: 'ts-app icon-doraemon',
-    title: 'Loader'
-  },
+  // {
+  //   route: 'loading-test',
+  //   icon: 'ts-app icon-doraemon',
+  //   title: 'Loader'
+  // },
   {
     icon: 'ts-app icon-brand',
     title: 'Logos',
