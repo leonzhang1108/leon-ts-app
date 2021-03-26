3d4dd9d33a0127e9450f81d682ef4c7858204851
leonzhang <human1108@163.com>
2021-03-26 23:57:15 +0800

    feat: 样式秀爱

--- src/pages/Mando/index.tsx
+++ src/pages/Mando/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import Utils from '@utils'
 import './index.less'
 
-const Mando = () => {
+const Mando = (props: any) => {
+  const { isMobile } = props
   return (
     <div className="mando-wrapper">
-      <div className="casco-top">
-        <div className="rayas-casco" />
-      </div>
-      <div className="linea" />
-      <div className="cara">
-        <div className="visor" />
-        <div className="visor-abajo" />
-        <div className="circulo-1" />
-        <div className="circulo-2" />
-        <div className="costado-1" />
-        <div className="costado-2" />
-        <div className="corte-casco-1" />
-        <div className="corte-casco-2" />
+      <div className={`icon-wrapper ${isMobile ? '' : 'pc'}`}>
+        <div className="casco-top">
+          <div className="rayas-casco" />
+        </div>
+        <div className="linea" />
+        <div className="cara">
+          <div className="visor" />
+          <div className="visor-abajo" />
+          <div className="circulo-1" />
+          <div className="circulo-2" />
+          <div className="costado-1" />
+          <div className="costado-2" />
+          <div className="corte-casco-1" />
+          <div className="corte-casco-2" />
+        </div>
       </div>
       <div className="text">
         <h2 className="title">
@@ -28,4 +32,10 @@ const Mando = () => {
   )
 }
 
-export default Mando
+export default Utils.connect({
+  component: Mando,
+  mapStateToProps: state => ({
+    isMobile: state.common.isMobile
+  })
+})
+
