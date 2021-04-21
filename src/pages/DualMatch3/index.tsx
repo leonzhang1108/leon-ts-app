ca531026a7c6a84fe6234dd23ecd9d4355aaf829
leonzhang <human1108@163.com>
2021-04-22 01:55:25 +0800

    upfeat

--- src/pages/DualMatch3/index.tsx
+++ src/pages/DualMatch3/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useCallback, useRef, useMemo } from 'react'
 import Utils from '@utils'
+import NotAvailableOnMobile from '@cpt/NotAvailableOnMobile'
 import './index.less'
 
 const getItems = () => {
@@ -24,7 +25,9 @@ const DualMatch3 = (props: any) => {
   const [itemList, setItemList] = useState(getItems())
   const [draging, setDraging] = useState(false)
   const [comboList, setComboList] = useState<any>([])
-  const [{ x, y }, setMousePos] = useState({ x: 50, y: 50 })
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
+
+  const { x, y } = mousePos
 
   const containerStyle = useMemo(() => {
     return {
@@ -35,12 +38,26 @@ const DualMatch3 = (props: any) => {
 
   const itemStyle = useCallback(({ row, col }) => {
     return {
-      top: row * (isMobile ? 40: 60),
-      left: col * (isMobile ? 40: 60),
+      top: row * (isMobile ? 40 : 60),
+      left: col * (isMobile ? 40 : 60),
       width: isMobile ? 30 : 50,
       height: isMobile ? 30 : 50
     }
   }, [isMobile])
+
+  const svgLinePoints = useMemo(() => {
+    if (!draging) return []
+    if (!isMobile) {
+      const { left: gameLeft, top: gameTop } = gameRef.current.getBoundingClientRect()
+      const { left: wrapperLeft, top: wrapperTop } = wrapperRef.current.getBoundingClientRect()
+      const positions = comboList.map((item: any) => {
+        const { row, col } = item
+        return { x: col * 60 - wrapperLeft + gameLeft + 25, y: row * 60 - wrapperTop + gameTop + 25 }
+      })
+      return [...positions, mousePos]
+    }
+    return []
+  }, [draging, comboList, mousePos, isMobile])
 
   useEffect(() => {
     // console.log(draging)
@@ -64,9 +81,7 @@ const DualMatch3 = (props: any) => {
     }
   }, [comboList, draging])
 
-  const wrapperMouseMove = useCallback((e) => {
-    if (!draging) return
-    const { left: gameLeft, top: gameTop } = gameRef.current.getBoundingClientRect()
+  const doSetMousePosition = (e) => {
     const { left: wrapperLeft, top: wrapperTop } = wrapperRef.current.getBoundingClientRect()
     const mouseLeft = e.pageX || e.touches[0].pageX
     const mouseTop = e.pageY || e.touches[0].pageY
@@ -74,37 +89,55 @@ const DualMatch3 = (props: any) => {
     const left = mouseLeft - wrapperLeft
     const top = mouseTop - wrapperTop
     setMousePos({ x: left, y: top })
+  }
+
+  const wrapperMouseMove = useCallback((e) => {
+    if (!draging) return
+    doSetMousePosition(e)
   }, [comboList, draging])
 
-  return (
+  return !isMobile ? (
     <div
       className="dual-match-3-wrapper"
       ref={wrapperRef}
       onMouseUp={() => setDraging(false)}
-      onTouchEnd={() => setDraging(false)}
       onMouseMove={wrapperMouseMove}
-      onTouchMove={wrapperMouseMove}
     >
       <svg className="svg-container" xmlns="http://www.w3.org/2000/svg" version="1.1">
-        <line x1="50" y1="50" x2={x} y2={y} style={{ stroke: 'red', strokeWidth: 5 }}/>
+        {
+          svgLinePoints.map((points, i) => {
+            if (i !== 0) {
+              const prevPoints = svgLinePoints[i - 1]
+              // console.log(points, prevPoints)
+              return (
+                <line
+                  key={i}
+                  x1={prevPoints.x}
+                  y1={prevPoints.y}
+                  x2={points.x}
+                  y2={points.y}
+                  style={{ stroke: 'red', strokeWidth: 5 }}
+                />
+              )
+            }
+            return null
+          })
+        }
       </svg>
       <div className="game-area" ref={gameRef} style={containerStyle}>
         {
           itemList.map(
-            (list: any, row: number) => 
+            (list: any, row: number) =>
               list.map(
                 (item: any, col: number) => (
                   <div
                     className="item"
                     key={item.key}
                     style={itemStyle({ row, col })}
-                    onMouseDown={() => {
+                    onMouseDown={(e) => {
                       setDraging(true)
-                      setComboList(comboList => comboList.concat({ row, col, key: item.key }))
-                    }}
-                    onTouchStart={() => {
-                      setDraging(true)
-                      setComboList(comboList => comboList.concat({ row, col, key: item.key }))
+                      doSetMousePosition(e)
+                      setComboList(() => [{ row, col, key: item.key }])
                     }}
                     onMouseEnter={() => mouseEnter({ row, col, key: item.key })}
                   ></div>
@@ -114,7 +147,7 @@ const DualMatch3 = (props: any) => {
         }
       </div>
     </div>
-  )
+  ) : <NotAvailableOnMobile />
 }
 
 export default Utils.connect({
