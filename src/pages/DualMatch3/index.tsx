9ee86672e295a187ddf05708c4f05a2eb7cb2b52
Leon <871162172@qq.com>
2021-04-22 19:33:39 +0800

    Update index.tsx

--- src/pages/DualMatch3/index.tsx
+++ src/pages/DualMatch3/index.tsx
@@ -8,17 +8,19 @@ const colors = ['#4E79A5', '#F18F3B', '#E0585B', '#77B7B2']
 const randomNum = (min, max) =>
   parseInt(Math.random() * (max - min + 1) + min, 10)
 
+const initItem = () => ({
+  key: Utils.uuid(),
+  color: colors[randomNum(0, 3)],
+  value: randomNum(1, 3),
+  opacity: 1
+})
+
 const getItems = () => {
   const list: any = []
   for (let i = 0; i < 6; i++) {
     const itemList: any = []
     for (let j = 0; j < 6; j++) {
-      itemList.push({
-        key: Utils.uuid(),
-        color: colors[randomNum(0, 3)],
-        value: randomNum(1, 3),
-        opacity: 1
-      })
+      itemList.push(initItem())
     }
     list.push(itemList)
   }
@@ -27,9 +29,24 @@ const getItems = () => {
 
 const getItemClass = (row, col, comboList) => {
   const index = comboList.findIndex((item) => {
-    return item.row === row && item.col === col
+    return Number(item.row) === Number(row) && Number(item.col) === Number(col)
   })
   return index >= 0 ? 'item active' : 'item'
+}
+
+const calculateNewItemList = (list) => {
+  for (let col = 0; col < list.length; col++) {
+    const colList = list.map(item => item[col]).filter(item => item.opacity === 1)
+    const newItemsList: any = []
+    for (let num = 0; num < list.length - colList.length; num++) {
+      newItemsList.push(initItem())
+    }
+    ;[...newItemsList, ...colList].map((item, i) => {
+      const { top, ...rest } = item
+      list[i][col] = rest
+    })
+  }
+  return [...list]
 }
 
 const DualMatch3 = (props: any) => {
@@ -37,13 +54,12 @@ const DualMatch3 = (props: any) => {
 
   const wrapperRef = useRef<any>()
   const gameRef = useRef<any>()
+  const staticItem = useRef<any>()
   const [itemList, setItemList] = useState(getItems())
   const [draging, setDraging] = useState(false)
   const [comboList, setComboList] = useState<any>([])
   const [lineColor, setLineColor] = useState<string>()
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
-
-  const { x, y } = mousePos
 
   const containerStyle = useMemo(() => {
     return {
@@ -54,14 +70,14 @@ const DualMatch3 = (props: any) => {
 
   const itemStyle = useCallback(
     ({ row, col, item }) => {
-      const { color, opacity } = item
+      const { color, opacity, top = 0 } = item
       return {
-        top: row * (isMobile ? 40 : 60),
+        top: row * (isMobile ? 40 : 60) + top,
         left: col * (isMobile ? 40 : 60),
         width: isMobile ? 30 : 40,
         height: isMobile ? 30 : 40,
         background: color,
-        opacity
+        opacity,
       }
     },
     [isMobile]
@@ -69,25 +85,22 @@ const DualMatch3 = (props: any) => {
 
   const svgLinePoints = useMemo(() => {
     if (!draging) return []
-    if (!isMobile) {
-      const {
-        left: gameLeft,
-        top: gameTop,
-      } = gameRef.current.getBoundingClientRect()
-      const {
-        left: wrapperLeft,
-        top: wrapperTop,
-      } = wrapperRef.current.getBoundingClientRect()
-      const positions = comboList.map((item: any) => {
-        const { row, col } = item
-        return {
-          x: col * 60 - wrapperLeft + gameLeft + 20,
-          y: row * 60 - wrapperTop + gameTop + 20,
-        }
-      })
-      return [...positions, mousePos]
-    }
-    return []
+    const {
+      left: gameLeft,
+      top: gameTop,
+    } = gameRef.current.getBoundingClientRect()
+    const {
+      left: wrapperLeft,
+      top: wrapperTop,
+    } = wrapperRef.current.getBoundingClientRect()
+    const positions = comboList.map((item: any) => {
+      const { row, col } = item
+      return {
+        x: col * (isMobile ? 40 : 60) - wrapperLeft + gameLeft + (isMobile ? 15 : 20),
+        y: row * (isMobile ? 40 : 60) - wrapperTop + gameTop + (isMobile ? 15 : 20),
+      }
+    })
+    return [...positions, mousePos]
   }, [draging, comboList, mousePos, isMobile])
 
   const mouseEnter = useCallback(
@@ -125,7 +138,7 @@ const DualMatch3 = (props: any) => {
 
   const wrapperMouseMove = useCallback(
     (e) => {
-      if (!draging) return
+      if (!draging || isMobile) return
       doSetMousePosition(e)
     },
     [comboList, draging]
@@ -133,26 +146,77 @@ const DualMatch3 = (props: any) => {
 
   const mouseUp = useCallback(() => {
     if (comboList.length >= 3) {
-      const tempList = [...itemList]
+
+      // hide combo item
       comboList.forEach((item) => {
         const { row, col } = item
-        tempList[row][col] = {
-          ...tempList[row][col],
-          opacity: 0
-        }
+        itemList[row][col].opacity = 0
       })
-      setItemList(itemList)
+
+      // gravity effect
+      const tempList: any = [[], [], [], [], [], []]
+      for (let col = 0; col < 6; col++) {
+        let top = 0
+        let row = 5
+        let list: any = []
+
+        while (row >= 0) {
+          const item = itemList[row][col]
+          item.top = top
+          if (item.opacity === 0) {
+            top += (isMobile ? 40 : 60)
+          }
+          row--
+          list = [item, ...list]
+        }
+        
+        list.forEach((item: any, row) => {
+          tempList[row].push({ ...item })
+        })
+      }
+
+      setItemList(tempList)
+
+      setTimeout(() => {
+        const list = calculateNewItemList(itemList)
+        setItemList(list)
+      }, 500)
     }
     setDraging(false)
     setComboList([])
   }, [comboList, itemList])
 
-  return !isMobile ? (
+  const touchMove = e => {
+    if (!isMobile) return
+    const touch = e.targetTouches[0]
+    const currTarget: any = document.elementFromPoint(Math.floor(touch.pageX), Math.floor(touch.pageY))
+    if (currTarget.className === 'item') {
+      if (!staticItem.current) {
+        const col = currTarget.getAttribute('col')
+        const row = currTarget.getAttribute('row')
+        const value = currTarget.getAttribute('value')
+        const color = currTarget.getAttribute('color')
+        const key = currTarget.getAttribute('unique')
+        staticItem.current = {
+          col, row, value, key
+        }
+        const item = { color, key }
+        mouseEnter({ row, col, item })
+      }
+    } else {
+      staticItem.current = null
+    }
+    doSetMousePosition(e)
+  }
+
+  return (
     <div
       className="dual-match-3-wrapper"
       ref={wrapperRef}
       onMouseUp={mouseUp}
       onMouseMove={wrapperMouseMove}
+      onTouchMove={touchMove}
+      onTouchEnd={mouseUp}
     >
       <svg
         className="svg-container"
@@ -183,13 +247,28 @@ const DualMatch3 = (props: any) => {
               className={getItemClass(row, col, comboList)}
               key={item.key}
               style={itemStyle({ row, col, item })}
+              // @ts-ignore
+              row={row}
+              col={col}
+              color={item.color}
+              value={item.value}
+              unique={item.key}
               onMouseDown={(e) => {
+                if (isMobile) return
                 setDraging(true)
                 doSetMousePosition(e)
                 setComboList(() => [{ row, col, key: item.key }])
                 setLineColor(item.color)
               }}
-              onMouseEnter={() => mouseEnter({ row, col, item })}
+              onTouchStart={(e) => {
+                if (!isMobile) return
+                setDraging(true)
+                doSetMousePosition(e)
+                setComboList(() => [{ row, col, key: item.key }])
+                setLineColor(item.color)
+                e.preventDefault()
+              }}
+              onMouseEnter={() => !isMobile && mouseEnter({ row, col, item })}
             >
               {item.value}
             </div>
@@ -197,8 +276,6 @@ const DualMatch3 = (props: any) => {
         )}
       </div>
     </div>
-  ) : (
-    <NotAvailableOnMobile />
   )
 }
 
