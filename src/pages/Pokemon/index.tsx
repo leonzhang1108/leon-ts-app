fb1817959b0277f3b1151fbf8fd22fd245866308
Leon <871162172@qq.com>
2021-07-01 16:10:17 +0800

    Create index.tsx

---
+++ src/pages/Pokemon/index.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef, useState, useMemo } from 'react'
+import init from './util'
+import Utils from '@utils'
+import Loader from '@cpt/Loader'
+import './index.less'
+
+const Pokemon = (props: any) => {
+  const wrapperRef = useRef<any>()
+  const { height, width } = props
+  const [count, setCount] = useState(0)
+
+  const loading = useMemo(() => {
+    return count !== 4
+  }, [count])
+
+  useEffect(() => {
+    init({
+      el: wrapperRef.current,
+      height,
+      width,
+      setCount,
+    })
+  }, [])
+
+  return (
+    <div className="pokemon-wrapper">
+      <div ref={wrapperRef} />
+      <div className="source-tip">
+        source: https://codepen.io/pizza3/pen/oNZYyxy
+      </div>
+      {loading ? (
+        <div className="model-loading">
+          <Loader />
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export default Utils.connect({
+  component: Pokemon,
+  mapStateToProps: (state) => ({
+    width: state.common.contentWidth,
+    height: state.common.contentHeight,
+  }),
+})
