1c10573578f540644cb07ccf012b784ceddefd73
Leon <871162172@qq.com>
2021-07-24 19:31:56 +0800

    Create index.tsx

---
+++ src/pages/Archery/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.less'
+
+const Archery = () => {
+  return (
+    <div className="archery-wrapper">
+      <div className="arrow" />
+      <div className="top">
+        <div className="arrow-inner" />
+        <div className="top-inner" />
+      </div>
+    </div>
+  )
+}
+
+export default Archery
