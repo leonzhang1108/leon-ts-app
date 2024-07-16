a01670e207328d46bdbc822cb794ebc7124d6161
张健亮 <zhangjianliang@zhangjianliangdeMacBook-Pro.local>
2024-07-17 01:41:09 +0800

    update

--- config/rules.js
+++ config/rules.js
@@ -1,83 +1,94 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const tsImportPluginFactory = require('ts-import-plugin')
 
-module.exports = [{
+module.exports = [
+  {
     test: /\.js$/,
-    loader: "babel-loader",
-    exclude: /node_modules/
+    loader: 'babel-loader',
+    exclude: /node_modules/,
   },
-  // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
   {
     test: /\.tsx?$/,
-    loader: "awesome-typescript-loader",
+    loader: 'ts-loader',
     options: {
-      useBabel: false, // !important!
+      transpileOnly: true,
       getCustomTransformers: () => ({
-        before: [tsImportPluginFactory({
-          libraryName: 'antd',
-          libraryDirectory: 'lib',
-          style: true
-        })]
+        before: [
+          tsImportPluginFactory({
+            libraryName: 'antd',
+            libraryDirectory: 'lib',
+            style: 'css',
+          }),
+        ],
       }),
+      compilerOptions: {
+        module: 'es2015',
+      },
     },
-    exclude: [
-      /node_modules\/mutationobserver-shim/g,
-    ]
+    exclude: [/node_modules\/mutationobserver-shim/g],
   },
   {
     test: /\.s(a|c)ss$/, // 匹配文件
-    use: [process.env.NODE_ENV === 'PROD' ? {
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          // you can specify a publicPath here
-          // by default it use publicPath in webpackOptions.output
-          publicPath: '../'
-        }
-      } : 'style-loader', // 使用<style>将css-loader内部样式注入到我们的HTML页面,
+    use: [
+      process.env.NODE_ENV === 'PROD'
+        ? {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../',
+            },
+          }
+        : 'style-loader', // 使用<style>将css-loader内部样式注入到我们的HTML页面,
       'css-loader', // 加载.css文件将其转换为JS模块
       {
         loader: 'postcss-loader',
         options: {
           config: {
-            path: './' // 写到目录即可，文件名强制要求是postcss.config.js
-          }
-        }
-      }, 'sass-loader'
-    ]
+            path: './', // 写到目录即可，文件名强制要求是postcss.config.js
+          },
+        },
+      },
+      'sass-loader',
+    ],
   },
   {
     test: /\.(le|c)ss$/, // 匹配文件
-    use: [process.env.NODE_ENV === 'PROD' ? {
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          // you can specify a publicPath here
-          // by default it use publicPath in webpackOptions.output
-          publicPath: '../'
-        }
-      } : 'style-loader', // 使用<style>将css-loader内部样式注入到我们的HTML页面,
+    use: [
+      process.env.NODE_ENV === 'PROD'
+        ? {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../',
+            },
+          }
+        : 'style-loader', // 使用<style>将css-loader内部样式注入到我们的HTML页面,
       'css-loader', // 加载.css文件将其转换为JS模块
       {
         loader: 'postcss-loader',
         options: {
           config: {
-            path: './' // 写到目录即可，文件名强制要求是postcss.config.js
-          }
-        }
+            path: './', // 写到目录即可，文件名强制要求是postcss.config.js
+          },
+        },
       },
       {
         loader: 'less-loader',
         options: {
-          lessOptions: { // 如果使用less-loader@5，请移除 lessOptions 这一级直接配置选项。
+          lessOptions: {
+            // 如果使用less-loader@5，请移除 lessOptions 这一级直接配置选项。
             javascriptEnabled: true,
           },
         },
-      }
-    ]
+      },
+    ],
   },
   {
     test: /\.(html)$/,
     use: {
-      loader: "html-loader",
+      loader: 'html-loader',
       options: {
         attributes: {
           list: [
@@ -95,38 +106,40 @@ module.exports = [{
               tag: 'audio',
               attribute: 'src',
               type: 'src',
-            }
-          ]
+            },
+          ],
         },
-        minimize: true
-      }
-    }
+        minimize: true,
+      },
+    },
   },
   {
     test: /\.(png|svg|jpe?g|gif)$/i, // 图片处理
-    use: [{
-      loader: "url-loader",
-      options: {
-        name: "[name].[hash:5].[ext]",
-        limit: 20 * 1024, // size <= 50kb
-        outputPath: "img"
-      }
-    }]
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          name: '[name].[hash:5].[ext]',
+          limit: 20 * 1024, // size <= 50kb
+          outputPath: 'img',
+        },
+      },
+    ],
   },
   {
     test: /\.(woff|woff2|eot|ttf|otf)$/, // 字体处理
-    use: ["file-loader"]
+    use: ['file-loader'],
   },
   {
     test: /\.(mp3|ogg|wav|wma|ape|aac)$/i, // 音频处理
-    use: ["file-loader"]
+    use: ['file-loader'],
   },
   {
     test: /\.(csv|tsv)$/, // 文件处理
-    use: ["csv-loader"]
+    use: ['csv-loader'],
   },
   {
     test: /\.xml$/, // 文件处理
-    use: ["xml-loader"]
-  }
+    use: ['xml-loader'],
+  },
 ]
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const rules = require('./rules')
 const isProd = process.env.NODE_ENV === 'PROD'
-const { TsConfigPathsPlugin } = require('awesome-typescript-loader')
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const configFile = path.resolve(__dirname, '../tsconfig.json')
 
 module.exports = {
@@ -18,15 +18,15 @@ module.exports = {
     // 输出路径
     path: path.resolve(__dirname, '../dist'),
     // 资源请求路径
-    publicPath: isProd ? './' : ''
+    publicPath: isProd ? './' : '',
   },
   module: {
-    rules
+    rules,
   },
   plugins: [
     // 清除文件
     new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['dist']
+      cleanAfterEveryBuildPatterns: ['dist'],
     }),
     // 提取样式文件
     new MiniCssExtractPlugin({
@@ -40,92 +40,108 @@ module.exports = {
       filename: './index.html',
       favicon: './public/favicon.ico',
       chunksSortMode: 'none',
-      hash: true
+      hash: true,
     }),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, '../public/icon'),
-          to: path.resolve(__dirname, '../dist/icon')
+          to: path.resolve(__dirname, '../dist/icon'),
         },
         {
           from: path.resolve(__dirname, '../public/3d-models'),
-          to: path.resolve(__dirname, '../dist/3d-models')
+          to: path.resolve(__dirname, '../dist/3d-models'),
         },
-        { 
+        {
           from: path.resolve(__dirname, '../public/manifest.json'),
-          to: path.resolve(__dirname, '../dist/manifest.json')
+          to: path.resolve(__dirname, '../dist/manifest.json'),
         },
-      ]
-    })
+      ],
+    }),
   ],
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json', '.png', '.jpg', '.mp3', '.ogg', 'svg'],
-    plugins: [ new TsConfigPathsPlugin({ configFile }) ],
+    extensions: [
+      '.ts',
+      '.tsx',
+      '.js',
+      '.json',
+      '.png',
+      '.jpg',
+      '.mp3',
+      '.ogg',
+      'svg',
+    ],
+    plugins: [new TsConfigPathsPlugin({ configFile })],
     alias: {
-      '@ant-design/icons/lib$': path.resolve(__dirname, '../src/constant/icons.ts')
-    }
+      '@ant-design/icons/lib$': path.resolve(
+        __dirname,
+        '../src/constant/icons.ts'
+      ),
+    },
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',   // initial、async和all
-      minSize: 30000,   // 形成一个新代码块最小的体积
-      maxAsyncRequests: 5,   // 按需加载时候最大的并行请求数
-      maxInitialRequests: 3,   // 最大初始化请求数
-      automaticNameDelimiter: '~',   // 打包分割符
+      chunks: 'all', // initial、async和all
+      minSize: 30000, // 形成一个新代码块最小的体积
+      maxAsyncRequests: 5, // 按需加载时候最大的并行请求数
+      maxInitialRequests: 3, // 最大初始化请求数
+      automaticNameDelimiter: '~', // 打包分割符
       name: true,
       cacheGroups: {
-        vendors: { // 基本框架
+        vendors: {
+          // 基本框架
           chunks: 'all',
           test: /(antd|react|react-dom|react-dom-router|redux|react-loadable)/,
           priority: 100,
           name: 'vendors',
         },
-        d3Vendor: { // 将体积较大的d3单独提取包，指定页面需要的时候再异步加载
+        d3Vendor: {
+          // 将体积较大的d3单独提取包，指定页面需要的时候再异步加载
           test: /d3/,
           priority: 100, // 设置高于async-commons，避免打包到async-common中
           name: 'd3Vendor',
-          chunks: 'async'
+          chunks: 'async',
         },
-        echartsVendor: { // 异步加载echarts包
+        echartsVendor: {
+          // 异步加载echarts包
           test: /(echarts|zrender)/,
           priority: 100, // 高于async-commons优先级
           name: 'echartsVendor',
-          chunks: 'async'
+          chunks: 'async',
         },
         trackingVendor: {
           test: /tracking/,
           priority: 100,
           name: 'trackingVendor',
-          chunks: 'async'
+          chunks: 'async',
         },
         flubberVendor: {
           test: /flubber/,
           priority: 100,
           name: 'flubberVendor',
-          chunks: 'async'
+          chunks: 'async',
         },
         particle: {
           test: /react-particles-js/,
           priority: 100,
           name: 'particleJsVendor',
-          chunks: 'async'
+          chunks: 'async',
         },
         matterjs: {
           test: /matter-js/,
           priority: 100,
           name: 'matterjsVendor',
-          chunks: 'async'
+          chunks: 'async',
         },
         three: {
           test: /three/,
           priority: 100,
           name: 'threejsVendor',
-          chunks: 'async'
+          chunks: 'async',
         },
-      }
-    }
+      },
+    },
   },
-  stats: { children: false }
+  stats: { children: false },
 }
