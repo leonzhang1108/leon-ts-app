7f77fd8a27ccd2baf107e10e9eebab629f5f19f2
张健亮 <zhangjianliang@zhangjianliangdeMacBook-Pro.local>
2024-07-17 23:50:11 +0800

    update

--- src/pages/5123/index.tsx
+++ src/pages/5123/index.tsx
@@ -1,12 +1,28 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useRef } from 'react'
+import { Howl } from 'howler'
 import init from './index.js'
 import './index.less'
 
 const Page5123 = () => {
+  const audioRef: any = useRef()
   useEffect(() => {
     init()
+
+    window.addEventListener('keydown', () => {
+      audioRef.current.play()
+    })
   }, [])
-  return <canvas id="c" />
+
+  return (
+    <div>
+      <canvas id="c" />
+      <audio
+        ref={audioRef}
+        src="https://music.163.com/song/media/outer/url?id=566436186.mp3"
+        autoPlay
+      />
+    </div>
+  )
 }
 
 export default Page5123
